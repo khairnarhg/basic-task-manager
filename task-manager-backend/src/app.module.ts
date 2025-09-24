73a54d10bb603904d8b970e74be22337383ab409
khairnarhg <hgkhairnar255@outlook.com>
2025-09-25 02:41:40 +0530

    env changed

--- task-manager-backend/src/app.module.ts
+++ task-manager-backend/src/app.module.ts
@@ -11,11 +11,12 @@ import { TasksModule } from './tasks/tasks.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      // host: process.env.DATABASE_HOST,
+      // port: Number(process.env.DATABASE_PORT),
+      // username: process.env.DATABASE_USER,
+      // password: process.env.DATABASE_PASSWORD,
+      // database: process.env.DATABASE_NAME,
+      url: process.env.   ,
       autoLoadEntities: true,
       // eslint-disable-next-line prettier/prettier
       synchronize: true, 
